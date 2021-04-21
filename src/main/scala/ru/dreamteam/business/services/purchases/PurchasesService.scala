8a8776c1bb04138095b10f44d88b2a74a5ac3b80
donilNick <danil.gorbachev0@gmail.com>
2021-04-21 20:47:49 +0500

    realization purchasesService

--- src/main/scala/ru/dreamteam/business/services/purchases/PurchasesService.scala
+++ src/main/scala/ru/dreamteam/business/services/purchases/PurchasesService.scala
@@ -6,6 +6,10 @@ import ru.dreamteam.business.{Money, Purchase, User}
 trait PurchasesService[F[_]] {
 
   // дописать создать, все на ваше усмотрение
+  /* Д: пока решили остановиться на этих методах, потом можно добавить выдачу покупок за какой-нибудь промежуток времени
+  или вообще комбинацию некоторых методов: выдать за определенный промежуток определенного типа или выдача сразу
+  нескольких типов
+  */
   def getPurchase(userId: User.Id): F[List[Purchase]]
 
   def getPurchaseByType(userId: User.Id, purchaseType: PurchaseType): F[List[Purchase]]
--- src/main/scala/ru/dreamteam/business/services/purchases/interpreter/PurchasesServiceInterpreter.scala
+++ src/main/scala/ru/dreamteam/business/services/purchases/interpreter/PurchasesServiceInterpreter.scala
@@ -1,14 +1,21 @@
 package ru.dreamteam.business.services.purchases.interpreter
 
-import ru.dreamteam.business.Purchase.PurchaseType
+import ru.dreamteam.business.Purchase.{Category, Comment, PurchaseType}
 import ru.dreamteam.business.{Money, Purchase, User}
 import ru.dreamteam.business.backends.purchases.PurchasesRepository
+import ru.dreamteam.business.backends.purchases.PurchasesRepository.PurchaseRequest
 import ru.dreamteam.business.services.purchases.PurchasesService
 
 class PurchasesServiceInterpreter[F[_]](repo: PurchasesRepository[F]) extends PurchasesService[F] {
-  override def getPurchase(userId: User.Id): F[List[Purchase]] = ???
+  implicit def purchaseTypeToCategory(purchaseType: PurchaseType): Category = Category(purchaseType)
+  //там написано, что лишняя вложенность
+  // пока такая затычка, потом когда уберем, можно просто передать метод repo под PurchaseType
 
-  override def getPurchaseByType(userId: User.Id, purchaseType: PurchaseType): F[List[Purchase]] = ???
+  implicit def stringToPurchaseComment(str: String): Comment = Comment(str)
 
-  override def addPurchase(userId: User.Id, money: Money, comment: String, `type`: PurchaseType): Unit = ???
+  override def getPurchase(userId: User.Id): F[List[Purchase]] = repo.getAllPurchases(userId)
+
+  override def getPurchaseByType(userId: User.Id, purchaseType: PurchaseType): F[List[Purchase]] = repo.getPurchasesByCategory(userId, purchaseType)
+
+  override def addPurchase(userId: User.Id, money: Money, comment: String, `type`: PurchaseType): Unit = repo.addPurchase(userId, PurchaseRequest(money, comment, `type`))
 }
