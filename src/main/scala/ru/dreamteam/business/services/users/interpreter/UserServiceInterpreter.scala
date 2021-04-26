689d04da15ebbbf1df914a838744b0738c088760
donilNick <danil.gorbachev0@gmail.com>
2021-04-26 15:48:27 +0500

    realization method login

--- src/main/scala/ru/dreamteam/business/services/users/interpreter/UserServiceInterpreter.scala
+++ src/main/scala/ru/dreamteam/business/services/users/interpreter/UserServiceInterpreter.scala
@@ -3,33 +3,37 @@ package ru.dreamteam.business.services.users.interpreter
 import cats.Monad
 import cats.effect.{BracketThrow, Sync}
 import cats.syntax.all._
-import doobie.implicits.toSqlInterpolator
-import ru.dreamteam.business.User.Password
-import ru.dreamteam.business.repository.purchases.interpreter.PurchaseRepositoryInterpreter.PurchaseRaw
-import ru.dreamteam.business.services.session.SessionService
-import ru.dreamteam.business.{Token, User}
-import ru.dreamteam.business.services.users.UserService
 import ru.dreamteam.business.repository.users.UsersRepository
-import ru.dreamteam.business.repository.users.interpreter.UsersRepositoryInterpreter.UserRaw
+import ru.dreamteam.business.services.session.SessionService
+import ru.dreamteam.business.services.users.UserService
+import ru.dreamteam.business.{Token, User}
 
-class UserServiceInterpreter[F[_]: Sync: BracketThrow: Monad](
-//  sessionService: SessionService[F],
-//  repo: UsersRepository[F]
-) extends UserService[F] {
+class UserServiceInterpreter[F[_] : Sync : BracketThrow : Monad](repo: UsersRepository[F], sessionService: SessionService[F]) extends UserService[F] {
 
   override def login(login: User.Login, password: User.Password): F[Token] =
-//    for {
-//      fromBd <- Sync[F].delay { User(???, ???) } // repo.get
-//      // проверить пароль
-//      isPasswordCorrect <- repo.checkPassword(login, password)
-//      token <- sessionService.generate(login)
-//    } yield Token (???) // token
-    ???
+    for {
+      fromBd <- repo.findUserByLogin(login).flatMap {
+        case Some(value) => Sync[F].delay(value)
+        case None => Sync[F].raiseError(LoginNotExist(s"$login don't exists"))
+      }
+      loggedUser <- checkUserPassword(fromBd, password) match {
+        case true => Sync[F].delay(fromBd)
+        case false => Sync[F].raiseError(PasswordNotCorrect(s"password don't correct"))
+      }
+      tokenString <- sessionService.generate(loggedUser.login)
+    } yield tokenString
+
+  def checkUserPassword(user: User, inputPassword: User.Password): Boolean = user.password == inputPassword
 
   override def registration(login: User.Login, password: User.Password): F[User] = ???
 
-  override def userInfo(): F[String] = Sync[F].raiseError(LoginExist("not bad"))
+  //override def userInfo(): F[String] = Sync[F].raiseError(LoginExist("not bad"))
 }
 
 abstract class BusinessError(msg: String, th: Throwable = null) extends Exception(msg, th)
+
 case class LoginExist(msg: String) extends BusinessError(msg)
+
+case class PasswordNotCorrect(msg: String) extends BusinessError(msg)
+
+case class LoginNotExist(msg: String) extends BusinessError(msg)
