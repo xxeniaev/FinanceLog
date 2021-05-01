000fde349f6b00ade2f490db1a0b41ec11036c31
donilNick <danil.gorbachev0@gmail.com>
2021-05-01 13:58:26 +0500

    realization SessionServiceInterpreter and UserServiceInterpreter (not tested)

--- src/main/scala/ru/dreamteam/business/services/session/interpreter/SessionServiceInterpreter.scala
+++ src/main/scala/ru/dreamteam/business/services/session/interpreter/SessionServiceInterpreter.scala
@@ -1,6 +1,8 @@
 package ru.dreamteam.business.services.session.interpreter
 
 import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import cats.implicits._
 import ru.dreamteam.business.services.session.SessionService
 import ru.dreamteam.business.{Token, User}
 
@@ -11,17 +13,38 @@ import scala.collection.immutable.Map
 // ref MVAR
 class SessionServiceInterpreter[F[_] : Sync]() extends SessionService[F] {
 
-  private val idTokenTable = Map.empty[Token, User] //переделать под ассинхронность
+  private val tokenTableRef: Ref[F, Map[Token, User]] = Ref.unsafe(Map.empty)
 
   override def generate(login: User.Login): F[Token] = Sync[F].delay {
-    Token(s"login:${login}")
+    Token(s"$login")
   }
 
-  override def getUserId(token: Token): F[User.Id] = Sync[F].delay {
-    idTokenTable.get(token)
-  } //переделать под асинхронность
+  override def getUserId(token: Token): F[Option[User.Id]] = {
+    val user = tokenTableRef.get.map(_.get(token)).map {
+      case Some(value) => Some(value.userId)
+      case None => None
+    }
+    user
+  }
+
+  override def addTokenUser(token: Token, user: User): F[Unit] = Sync[F].delay {
+    tokenTableRef.update(_ + (token -> user))
+    printTable()
+  }
 
   override def removeToken(token: Token): F[Unit] = Sync[F].delay {
-    idTokenTable.removed(token)
-  } //переделать под асинхронность
+    tokenTableRef.update(_ - token)
+    printTable()
+  }
+
+  private def printTable():Unit = {
+    print("______")
+    for {
+      keys <- tokenTableRef.get.map(_.keys)
+      s = keys.map(x => print(x, tokenTableRef.get.map(_.get(x))))
+    } yield Unit
+    print("______")
+  }
+
+
 }
--- src/main/scala/ru/dreamteam/business/services/users/interpreter/UserServiceInterpreter.scala
+++ src/main/scala/ru/dreamteam/business/services/users/interpreter/UserServiceInterpreter.scala
@@ -2,8 +2,10 @@ package ru.dreamteam.business.services.users.interpreter
 
 import cats.Monad
 import cats.effect.{BracketThrow, Sync}
+import cats.ApplicativeError
 import cats.syntax.all._
 import ru.dreamteam.business.repository.users.UsersRepository
+import ru.dreamteam.business.repository.users.UsersRepository.UserReq
 import ru.dreamteam.business.services.session.SessionService
 import ru.dreamteam.business.services.users.UserService
 import ru.dreamteam.business.{Token, User}
@@ -12,22 +14,22 @@ class UserServiceInterpreter[F[_] : Sync : BracketThrow : Monad](repo: UsersRepo
 
   override def login(login: User.Login, password: User.Password): F[Token] =
     for {
-      fromBd <- repo.findUserByLogin(login).flatMap {
-        case Some(value) => Sync[F].delay(value)
-        case None => Sync[F].raiseError(LoginNotExist(s"$login don't exists"))
-      }
-      loggedUser <- checkUserPassword(fromBd, password) match {
-        case true => Sync[F].delay(fromBd)
-        case false => Sync[F].raiseError(PasswordNotCorrect(s"password don't correct"))
-      }
-      tokenString <- sessionService.generate(loggedUser.login)
+      fromBd <- repo.findUserByLogin(login)
+      user <- Sync[F].fromOption(fromBd, LoginNotExist(s"$login not exist"))
+      _ <- Sync[F].raiseError(PasswordNotCorrect(s"$password not corrected")).whenA(!checkUserPassword(user, password))
+      tokenString <- sessionService.generate(user.login)
     } yield tokenString
 
   def checkUserPassword(user: User, inputPassword: User.Password): Boolean = user.password == inputPassword
 
-  override def registration(login: User.Login, password: User.Password): F[User] = ???
-
-  //override def userInfo(): F[String] = Sync[F].raiseError(LoginExist("not bad"))
+  override def registration(login: User.Login, password: User.Password): F[User] = for {
+    fromBd <- repo.findUserByLogin(login)
+    _ <- Sync[F].raiseError(LoginExist(s"$login exist")).whenA(fromBd.isEmpty)
+    userId <- repo.addUser(UserReq(login, password))
+    user = User(userId, login, password)
+    token <- sessionService.generate(login)
+    _ <- sessionService.addTokenUser(token, user)
+  } yield user
 }
 
 abstract class BusinessError(msg: String, th: Throwable = null) extends Exception(msg, th)
