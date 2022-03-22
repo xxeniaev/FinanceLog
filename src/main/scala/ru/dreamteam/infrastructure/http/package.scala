da4adce9583bbc287a068183c2d7bd71c81b328a
xxeniaev <xenia-ev2502@yandex.ru>
2022-03-22 21:37:02 +0500

    handle with authorization

--- src/main/scala/ru/dreamteam/infrastructure/http/package.scala
+++ src/main/scala/ru/dreamteam/infrastructure/http/package.scala
@@ -3,50 +3,47 @@ package ru.dreamteam.infrastructure
 import cats.syntax.all._
 import ru.dreamteam.business.{Token, User}
 import ru.dreamteam.business.services.session.SessionService
-import sttp.model.Header
-import sttp.tapir.Endpoint
+import sttp.tapir.{header, Endpoint}
 import sttp.tapir.server.ServerEndpoint
-import tethys.JsonWriterOps
-import tethys.commons.RawJson
-import tethys.jackson._
-import sttp.tapir._
-import sttp.tapir.model.ServerRequest
 import zio.{Has, IO, Task, UIO, ZIO, ZLayer}
 
 // попробуйте сами сделать метод, который достает хедер авторизационный, проверяет сессию, достает по ней пользователя
 package object http {
+
+  case object InvalidToken extends RuntimeException
+
   // подшаманить с типами
   implicit class EndpointReacher2[I, O](endpoint: Endpoint[I, Unit, Response[O], Any]) {
 
     def handleWithAuthorization(
-      logic: (User.Id, I) => ZIO[MainEnv, Throwable, O]
-    )(implicit sessionService: SessionService[MainTask]): ServerEndpoint[(I, ServerRequest), Unit, Response[O], Any, Task] =
-      ServerEndpoint[(I, ServerRequest), Unit, Response[O], Any, Task](
-        endpoint.in(extractFromRequest(identity)),
+      logic: I => User.Id => ZIO[MainEnv, Throwable, O]
+    )(
+      implicit
+      sessionService: SessionService[ZIO[MainEnv, Throwable, O]]
+    ): ServerEndpoint[(I, String), Unit, Response[O], Any, Task] =
+      ServerEndpoint[(I, String), Unit, Response[O], Any, Task](
+        endpoint.in(header[String]("session")),
         _ => {
-          case (i, request) => {
+          case (i, token) => {
             val r: ZIO[MainEnv, Nothing, Response[O]] = for {
+              trackingId <- ZIO.access[MainEnv](_.get[Context].trackingId)
+              idF         = sessionService.getUser(Token(token)).fold(Task.fail(InvalidToken), identity)
+              a          <- idF.flatMap(logic(i))
+                              .either.catchAllDefect(th => UIO(Left(th)))
+                              .map {
+                                case Left(value)  => Response(
+                                    none[O],
+                                    trackingId,
+                                    Status.Error,
+                                    ErrorPayload(value.getMessage).some
+                                  )
+                                case Right(value) =>
+                                  Response(value.some, trackingId, Status.Ok, none[ErrorPayload])
+                              }
+
               // взять заголовок, где лежит сессия, проверить что сессия у нас есть (SessionService), вернуть юзерайди пользователя
               // если нет сессии отбить ошибкой
 
-              trackingId <- ZIO.access[MainEnv](_.get[Context].trackingId)
-
-//              optHeader = request.header("x-auth-header").map(Token(_))
-//              optionUserId <- sessionService.getUser(???)
-              userId: User.Id = ???
-              result = logic(userId, i)
-              a          <- result.either
-                .catchAllDefect(th => UIO(Left(th)))
-                .flatMap {
-                  case Left(value)  => UIO(Response(
-                    none[O],
-                    trackingId,
-                    Status.Error,
-                    ErrorPayload(value.getMessage).some
-                  ))
-                  case Right(value) =>
-                    UIO(Response(value.some, trackingId, Status.Ok, none[ErrorPayload]))
-                }
             } yield a
 
             r.map(_.asRight[Unit]).provideLayer(ZLayer.fromFunction(_ =>
@@ -57,10 +54,6 @@ package object http {
       )
 
   }
-
-
-
-
 
   implicit class EndpointReacher[I, O](endpoint: Endpoint[I, Unit, Response[O], Any]) {
 
@@ -75,15 +68,15 @@ package object http {
               trackingId <- ZIO.access[MainEnv](_.get[Context].trackingId)
               a          <- logic(i).either
                               .catchAllDefect(th => UIO(Left(th)))
-                              .flatMap {
-                                case Left(value)  => UIO(Response(
+                              .map {
+                                case Left(value)  => Response(
                                     none[O],
                                     trackingId,
                                     Status.Error,
                                     ErrorPayload(value.getMessage).some
-                                  ))
+                                  )
                                 case Right(value) =>
-                                  UIO(Response(value.some, trackingId, Status.Ok, none[ErrorPayload]))
+                                  Response(value.some, trackingId, Status.Ok, none[ErrorPayload])
                               }
             } yield a
 
